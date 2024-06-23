68cbde10709057e714dc482fb8e67d0c033831a0
cfkim <christine.fth.kim@gmail.com>
2024-06-23 17:41:49 -0400

    Fix submit.php typo.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my-personal-website
-Since Github pages does not host non-static pages, I'm deciding to work on the website's contact form in this repository.
+Since Github pages does not host non-static pages, I'm deciding to work on the website's contact form in this repository. The static version of this website is hosted through Github Pages at this link: https://cfkim.github.io/
+
+What's different is that it does not have a contact form.
--- submit.php
+++ submit.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fname = htmlspecialchars($_POST['firstname']);
     $lname = htmlspecialchars($_POST['lastname']);
@@ -6,9 +9,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = htmlspecialchars($_POST['subject']);
     $message = htmlspecialchars($_POST['message']);
 
-    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
-    $to = 'christine.fth.kim@gmail.com';
-    $headers = 'From: $email';
+    $body = "Name: $fname\nEmail: $email\nMessage:\n$message";
+    $to = "christine.fth.kim@gmail.com";
+    $headers = "From: $email";
 
     if(mail($to, $subject, $body, $headers)){
         header("Location: /thankyou.html");
@@ -17,6 +20,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Oops something went wrong!";
     }
 }else{
-    echo "Invalid request method. Please submit the form."
+    echo "Invalid request method. Please submit the form.";
 }
 ?>
